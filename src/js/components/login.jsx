479601172f72ebab20c38f8613140bbf443aa768
liudch <liudchu@jingoal.com>
2017-03-15 18:09:52 +0800

    Modify the login page

--- src/js/components/login.jsx
+++ src/js/components/login.jsx
@@ -2,14 +2,25 @@ let React = require('react');
 let ReactDOM = require('react-dom');
 const item = React.createClass({
   render: function() {
+    // 账号输入框
+    function Account() {
+        return <div><input type="text" placeholder="请输入登录账号"/></div>;
+    }
+
+    // 密码输入框
+    function Password() {
+        return <div><input type="password" placeholder="请输入登录密码"/></div>;
+    }
+
+    function Loginbutton() {
+        return <div><input type="button" value="登&nbsp;&nbsp;&nbsp;&nbsp;录" className="btn"/></div>;
+    }
+
     return (
       <div className="jst-login">
-        <div>
-        <input type="text" placeholder="请输入登录账号"/>
-        </div>
-        <div>
-        <input type="password" placeholder="请输入登录密码"/>
-        </div>
+        <Account/>
+        <Password/>
+        <Loginbutton/>
       </div>
     );
   }
